00650d4ea8672afcc52e0916034288334f2e0ee5
unknown <mahidakshana@gmail.com>
2020-07-11 18:30:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/atry/billMessFragment.java
@@ -0,0 +1,30 @@
+package com.example.atry;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class billMessFragment extends Fragment {
+
+
+
+    public billMessFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bill_mess, container, false);
+    }
+}
